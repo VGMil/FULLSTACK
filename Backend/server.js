74c5047cbd9d9a2751a26f3b357adacd43844829
vgmil <vgmiltonisaac@gmail.com>
2025-05-03 13:32:54 -0500

    ✨ feat(server): agregar middleware CORS y actualizar configuración del servidor
    - Se añadió middleware CORS para permitir solicitudes cross-origin con opciones configurables
    - Actualización de la configuración del servidor para manejar solicitudes CORS preflight y estándar
    - Modificación de `server.js` para habilitar CORS con origen y métodos wildcard
    - Se añadieron ejemplos de uso de CORS y su integración con el middleware
    - Actualización de la documentación y ejemplos para reflejar la nueva funcionalidad CORS
    
    Este cambio mejora la capacidad del servidor para manejar solicitudes cross-origin, mejorando la compatibilidad con aplicaciones frontend.

--- Backend/server.js
+++ Backend/server.js
@@ -12,13 +12,16 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: "*",
     methods: ['GET', 'POST']
   }
 });
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST']
+}));
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
 
